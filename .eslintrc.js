b5e3955aa7642a690f7bef13f827c400009cece2
Ryohei <harrry.kuma1225@gmail.com>
2024-02-09 22:09:18 +0900

    fix eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,18 +5,25 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'unused-imports'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'plugin:import/recommended',
   ],
+  // ここで細かな設定を行うことができる
   settings: {
+    // 'import/parsers': {
+    //   '@typescript-eslint/parser': ['.ts', '.tsx'],
+    // },
+    // parsersは特に指定しなくても自動で設定されるし、変更点がないからこのままでいい
     'import/resolver': {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        moduleDirectory: ['node_modules', 'src/'],
+        moduleDirectory: ['node_modules', 'src'],
       },
+      // リゾルバは、ESLint が import 文で参照されるモジュールを見つけるために使用するツールです。リゾルバは、モジュールの名前とパスをマッピングし、ESLint が必要なモジュールを見つけられるようにします。
     },
   },
   root: true,
@@ -26,10 +33,14 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    complexity: ['error', 10],
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'error',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'warn',
     'import/order': [
       'error',
       {
@@ -58,7 +69,7 @@ module.exports = {
         alphabetize: {
           order: 'asc',
         },
-        // 'newlines-between': 'always',
+        'newlines-between': 'always',
       },
     ],
   },
